b446736858267fdbb5c91375ac5fb3a779080eae
skyv26 <dev.aakashv@gmail.com>
2022-08-27 01:24:03 +0530

    Add `class` for `API` call - Created class for API GET and POST method.

---
+++ utils/Api/Api.js
@@ -0,0 +1,26 @@
+class Api {
+  constructor() {
+    this.url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/JqyMjlAIfzoPkOxklElb/scores/';
+  }
+
+  promisedData = (obj = null) => {
+    const option = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify(obj),
+    };
+    return (async () => (await fetch(this.url, obj ? option : [])).json())();
+  }
+
+  get() {
+    return this.promisedData();
+  }
+
+  post(obj) {
+    this.promisedData(obj);
+  }
+}
+
+export default Api;
